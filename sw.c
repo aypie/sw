ce9a3150297256a022ce4eeaab7c86759b7e993d
Alex Potterson <alexeyrdoxi@gmail.com>
2020-08-12 22:19:53 +0300

    Fixed bugs

--- sw.c
+++ sw.c
@@ -10,6 +10,7 @@
 // Error codes
 enum {
 	ERROR_QUIT = 1,	// User prompted to quit, operation not finished
+	ERROR_NO_ARGS,	// No arguments were passed
 	ERROR_NO_PAIR,	// Some filename doesn't have a pair
 	ERROR_NO_FILE,	// Specified filename does not exist
 	ERROR_BUMP		// Bumped into a file that should not exist
@@ -52,7 +53,10 @@ int main(int ArgN, char *Args[]) {
 	signal(SIGINT, HandleSignals);
 
 	// If there are no arguments - show help
-	if (ArgN == 1) PrintHelp();
+	if (ArgN == 1) {
+		PrintHelp();
+		return ERROR_NO_ARGS;
+	}
 
 	// Check for --help and --version
 	for (int I = 0; I < ArgN; I++) {
@@ -79,12 +83,11 @@ int main(int ArgN, char *Args[]) {
 	}
 
 	// Check that every filename exists
-	for (int I = 1; I < ArgN; I++) {
+	for (int I = 1; I < ArgN; I++)
 		if (access(Args[I], F_OK) == -1) {
 			fprintf(stderr, "Error: filename \"%s\" does not exist.\n", Args[I]);
 			return ERROR_NO_FILE;
 		}
-	}
 
 	// Useful constants
 	const int Filenames = ArgN - 1;
@@ -92,17 +95,14 @@ int main(int ArgN, char *Args[]) {
 
 	// Fill the character lists
 	char *List[3][Filenames]; // 0=from 1=to 2=temp
-	for (int I = 0; I < Operations; I++) {
+	for (int I = 0; I < Operations; I++)
 		List[0][I] = Args[I+1];
-	}
-	for (int I = Operations; I < Filenames; I++) {
+	for (int I = Operations; I < Filenames; I++)
 		List[1][I - Operations] = Args[I+1];
-	}
-	for (int I = 0; I < Operations; I++) {
+	for (int I = 0; I < Operations; I++)
 		List[2][I] = NULL;	// So free() doesn't UB
-	}
 
-	// Move to temporary filenames
+	// Create temporary filenames
 	for (int I = 0; I < Operations; I++) {
 		List[2][I] = calloc(256, sizeof(char));
 		strcpy(List[2][I], List[0][I]);
@@ -114,18 +114,23 @@ int main(int ArgN, char *Args[]) {
 			}
 			return ERROR_BUMP;
 		}
-		rename(List[0][I], List[2][I]);
 	}
 
-	// Move to designated filenames
-	for (int I = 0; I < Operations; I++) {
+	// Move to temporary filenames
+	for (int I = 0; I < Operations; I++)
+		rename(List[0][I], List[2][I]);
+
+	// Move to old filenames
+	for (int I = 0; I < Operations; I++)
+		rename(List[1][I], List[0][I]);
+
+	// Move to new filenames
+	for (int I = 0; I < Operations; I++)
 		rename(List[2][I], List[1][I]);
-	}
 
 	// Freeing the memory before exit
-	for (int I = 0; I < Operations; I++) {
+	for (int I = 0; I < Operations; I++)
 		free(List[2][I]);
-	}
 
 	return 0;
 }
